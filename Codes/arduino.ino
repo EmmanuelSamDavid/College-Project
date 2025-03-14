77c2c67bd253a9569dd0d548e5a1aa2a35478526
Emmanuel Sam David D <131431172+EmmanuelSamDavid@users.noreply.github.com>
2025-03-14 10:49:39 +0530

    Add files via upload

---
+++ Codes/arduino.ino
@@ -0,0 +1,91 @@
+#include <Servo.h>
+#include <HX711_ADC.h> // need to install 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h> // need to install
+
+HX711_ADC LoadCell(4, 5); // parameters: dt pin 6, sck pin 7;
+LiquidCrystal_I2C lcd(0x27, 16,2); // 0x27 is the i2c address might different;you can check with Scanner
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+int relay=5;
+int buzzer=2;
+int gasflag=0;
+int levelflag=0;
+void setup() {
+  LoadCell.begin(); // start connection to HX711
+  LoadCell.start(2000); // load cells gets 2000ms of time to stabilize
+  LoadCell.setCalFactor(115.68); // calibration factor for load cell => dependent on your individual setup
+  lcd.init(); 
+  lcd.backlight();
+Serial.begin(115200);
+ myservo.attach(3);
+  myservo.write(pos);
+pinMode(buzzer,OUTPUT);
+  lcd.setCursor(0,0);
+  lcd.print("GAS MONITORING");
+   lcd.setCursor(0,1);
+  lcd.print("   SYSTEM   ");
+  delay(2000);
+  lcd.clear();
+   delay(6000);
+     lcd.print("System Ready");
+      delay(1000);
+  lcd.clear();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   int sensorValue = analogRead(A0);
+   LoadCell.update(); // retrieves data from the load cell
+  float i = LoadCell.getData(); // get output value
+   lcd.clear();
+ lcd.setCursor(0,0);
+  lcd.print("GAS:");
+   lcd.setCursor(5,0);
+  lcd.print(sensorValue);
+  lcd.setCursor(0,1);
+  lcd.print("WEIGHT:  ");
+   lcd.setCursor(7,1);
+  lcd.print(i);
+  if(sensorValue>475)
+  {
+    gasflag=1;
+     lcd.clear();
+     lcd.setCursor(0,0);
+  lcd.print("GAS DETECTED");
+  digitalWrite(buzzer,HIGH);
+  delay(300);
+  myservo.write(90);
+  }
+  else{
+    gasflag=0;
+    digitalWrite(buzzer,LOW);
+    myservo.write(pos);
+  }
+  if((i>20)&&(i<100))
+  {
+    levelflag=1;
+    lcd.clear();
+     lcd.setCursor(0,1);
+  lcd.print("GAS LEVEL:LOW  ");
+  digitalWrite(buzzer,HIGH);
+  }
+  else
+  {
+    levelflag=0;
+    digitalWrite(buzzer,LOW);
+  }
+       String getStr;
+  getStr +="&field1=";
+  getStr += String(gasflag);
+  getStr +="&field2=";
+  getStr += String(levelflag);
+  getStr +="&field3=";
+  getStr += String(i);
+  getStr += "\r\n\r\n";
+  Serial.print(getStr);
+  delay(300);
+}
+
---
+++ Codes/nodemcu.ino
@@ -0,0 +1,117 @@
+
+#include <Wire.h>
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include "ThingSpeak.h"
+#include "Pushover.h"
+const char* ssid = "iotdata";
+const char* password = "Indianss@12";
+unsigned long myChannelNumber =  2529961;
+const char * myWriteAPIKey = "XI5ZNRO5JCVH4N5Q";
+const char* pushoverToken = "ajaj5bmyxoft84zwtydrqfej5hmdkj";
+const char* pushoverUserKey = "u5zqsfu7tfombstw18hx6g6tv1nav3";
+WiFiClient  client;
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is completelag
+unsigned int spacePosition=0;
+String gas_status="";
+String level_status="";
+String level_value="";
+unsigned int count=0;
+void sendPushoverNotification(bool gasDetected,bool levelflag, float level) {
+  // Initialize Pushover
+  Pushover po = Pushover(pushoverToken, pushoverUserKey, UNSAFE);
+
+  if (gasDetected) {
+    // Create message with clickable Google Maps link
+    String message = "Gas Leakage detected!";
+
+    // Send Pushover notification
+    po.setMessage(message);
+    po.send();
+  }
+  if (levelflag) {
+    // Create message with clickable Google Maps link
+    String message = "Gas Level Near Empty!Automatic Booking Initiated!Status:Processing";
+
+    // Send Pushover notification
+    po.setMessage(message);
+    po.send();
+  }
+    ThingSpeak.setField(1, gasDetected);
+  ThingSpeak.setField(2,levelflag);
+  ThingSpeak.setField(3, level);
+     int m = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  inputString.reserve(200);
+  gas_status.reserve(10);
+ level_status.reserve(10);
+  level_value.reserve(10);
+
+  ThingSpeak.begin(client);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected");
+}
+ 
+void loop()
+{
+  while (Serial.available()) 
+  {
+    // get the new byte:up
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') 
+    {
+         stringComplete=true;
+    } 
+  
+  }
+     if(stringComplete)
+     {
+         spacePosition=inputString.indexOf("&field1=");
+         spacePosition+=8;
+               gas_status=inputString[spacePosition++];
+         spacePosition=inputString.indexOf("&field2=");
+         spacePosition+=8;
+               level_status=inputString[spacePosition++];
+           spacePosition=inputString.indexOf("&field3=");
+             spacePosition+=8;
+           for(count=0;count<6;count++)
+            {
+               level_value+=inputString[spacePosition++];
+            }
+                  }
+bool gasDetected = (gas_status.charAt(0) == '1');
+bool levelflag = (level_status.charAt(0) == '1');
+float level = level_value.substring(0, 12).toFloat();  // Assuming latitude is in the format xx.xxxxxx
+
+    sendPushoverNotification(gasDetected, levelflag, level);
+
+    gas_status="";
+   level_status="";
+   level_value="";
+
+    
+//    flow_value="";
+//    vol_value="";
+//    x_value="";
+//    y_value="";
+//    z_value="";
+        inputString = "";
+       stringComplete=false;
+ delay(1000);
+}
